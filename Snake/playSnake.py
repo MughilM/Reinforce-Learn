b8c5ede346439a98fda7f2a1dee922d8b12a37eb
Rangumi <mughilan.muthupari@gmail.com>
2020-09-21 13:06:51 -0400

    Removed the snake from the environment. The snake will now be the agent itself.

--- Snake/playSnake.py
+++ Snake/playSnake.py
@@ -1,31 +1,31 @@
-##################################################
+######################################################
 # File: playSnake.py
-# Author: Mughil Pari
+# Creation Date: 20/09/2020
 # Location: /Snake/
 #
-# This file implements the rules of the game Snake.
-# It includes methods to make moves, and also to
-# compile the entire game into an animated gif
-# for future viewing. This provides an environment
-# for which an agent can act upon. The state
-# of the game will be stored in matrices.
-# The environment simply stores the states
-# of the game and provides capability of
-# exporting the states into a playable GIF.
-##################################################
+# This file implements the simple environment
+# for the snake game. It only has the game board's
+# properties, because the snake can be considered an
+# agent himself. Thus, for the environment, we only
+# store the size of the board and the location of the
+# fruit. It also provides capability to export the
+# played game as a GIF, given the locations
+# of the snake at each frame.
+######################################################
 
 import numpy as np
 import imageio
-
 import os
 
 
 class SnakeGame:
     def __init__(self, boardSize=10):
         """
-        Creates the board, along with score, and random placement
-        of fruit. Also creates the export
-        directories if not present.
+        Saves the board size and randomly
+        places the fruit. Same functionality
+        as reset(), except the board size
+        is saved. Board size larger than 5 is
+        required.
         :param boardSize: Side length of board
         """
         # Create directories...
@@ -38,41 +38,64 @@ class SnakeGame:
         # Don't pay attention to the values here.
         # They'll get reset. It's just so my IDE can
         # recognize the data types used :)
-        self.score = 0
-        self.prevStates = []
-        self.currState = np.array([], dtype=float)
+        self.fruitLoc = (0, 0)
         self.reset()
 
     def reset(self):
         """
-        Resets the board. Randomly places first fruit and
-        places snake in top corner
-        :return:
+        Resets the board and places the snake
+        in the top corner. It will always
+        be of length 3. Randomly places the
+        fruit as well.
+        :return: Starting position of snake
         """
-        self.score = 3
-        self.prevStates = []
-        # We can make the initial position of the
-        # snake be in the top corner...
-        self.currState = np.zeros((self.boardSize, self.boardSize), dtype=float)
-        self.currState[0, :self.score] = 1
-        # The head of the snake will be slightly darker...
-        self.currState[0, self.score - 1] = 0.7
-        # Randomly place the fruit in the space...
-        fruitLoc = np.random.randint(low=self.score, high=self.boardSize ** 2)
-        self.currState[fruitLoc // self.boardSize, fruitLoc % self.boardSize] = 0.4
+        fruitCellNum = np.random.randint(low=3, high=self.boardSize ** 2)
+        self.fruitLoc = (fruitCellNum // self.boardSize, fruitCellNum % self.boardSize)
+        # Return top 3 corner cells...
+        return [(0, 0), (0, 1), (0, 2)]
 
-    def exportGIF(self, filename: str):
+    def exportGIF(self, filename: str, frames: list, fruitLocs: list):
         """
         Takes the previous states and exports them
         as a playable GIF. Automatically places
         it in the ./Data/gifs/ folder.
         :param: filename: Name of GIF file. Automatically
         prepended with ./Data/ and adds .gif if not there
+        :param: frames: A list containing the locations
+        of the snake at each frame. Expects each element
+        to be a list of two-tuples. The last element
+        is the snake head.
+        :param: frutiLocs: A list containing the fruit
+        locations at each frame. Should be the same length
+        as `frames`.
         :return:
         """
         if not filename.endswith('.gif'):
             filename += '.gif'
+        # We will convert these to numpy arrays
+        # An ndarray of shape
+        # (num of frames, board size + 2, board size + 2)
+        # We add 2 to the size to show the border...
+        # This also means we need to correct the indices.
+        # Border and snake body will be white. Snake
+        # head will half gray, and the fruit will be
+        # a darker gray.
+        playedGame = np.zeros(shape=(len(frames), self.boardSize + 2, self.boardSize + 2), dtype=float)
+        # Put the border...
+        playedGame[:, 0, :] = 1
+        playedGame[:, -1, :] = 1
+        playedGame[:, :, 0] = 1
+        playedGame[:, :, -1] = 1
         filename = os.path.join('./Data/', filename)
         with imageio.get_writer(filename, mode='I') as writer:
-            for state in self.prevStates:
-                writer.append_data(state)
+            for i, (frame, fruit) in enumerate(zip(frames, fruitLocs)):
+                # The format is the reverse of zip
+                correctedSnakeLocs = zip(*[(i, r + 1, c + 1) for r, c in frame])
+                correctedFruitLoc = (i, fruit[0] + 1, fruit[1] + 1)
+                playedGame[correctedSnakeLocs] = 1
+                playedGame[correctedFruitLoc] = 0.3
+                # Color the head...
+                playedGame[i, frame[-1][0] + 1, frame[-1][1] + 1] = 0.7
+
+                # Append to the GIF
+                writer.append_data(playedGame[i])
