6120584d5799472c142726cf402c5737605c012e
MughilM <mughilan.muthupari@gmail.com>
2021-02-13 11:18:06 -0500

    Added some more methods and documentation...

--- BaseClasses/agent.py
+++ BaseClasses/agent.py
@@ -11,7 +11,21 @@ and maybe a couple of properties, although that is up to the user.
 
 class Agent:
     def __init__(self):
+        self.reset()
         return
 
     def reset(self):
         raise NotImplementedError('Please implement a reset function for the agent!')
+
+    def makeMove(self, action, env):
+        return
+
+    def checkValidAction(self, action):
+        """
+        This function takes an action and verifies to make sure
+        the action is allowed. This could be as simple as checking
+        a defined actionList (if we have a discrete action space).
+        :param action: The action to take.
+        :return: True/False, if the action is allowed/not allowed.
+        """
+        raise NotImplementedError('Please provide a way to verify a provided action!')
--- BaseClasses/environment.py
+++ BaseClasses/environment.py
@@ -8,10 +8,39 @@ environments are extended from this one. At the bare minimum, an
 environment contains a reset method to return to the starting state.
 """
 
+from .agent import Agent
+from typing import Dict
+
 
 class Environment:
-    def __init__(self):
+    def __init__(self, agents: Dict[str, Agent]):
+        """
+        This environment allows for multiple agents
+        in the form of dictionaries. They should
+        be mapped from agent name to the actual agent
+        object
+        :param agents: A dictionary mapping the agent
+        name to the agent object.
+        """
+        self.agents = agents
+        self.reset()
         return
 
     def reset(self):
         raise NotImplementedError('Please implement a reset function for the environment!')
+
+    def stepForward(self, agentName, action):
+        """
+        The main function to make a step in the environment.
+        You must provide which agent's turn it is, and provide
+        the action. It will perform the action, and return
+        the resultant state, reward, and whether it resulted
+        in a game over.
+        :param agentName: The name of the agent, corresponding to the dictionary
+        :param action: The action to take.
+        :return: A 3-tuple of (resultant state, reward, game over flag)
+        """
+        raise NotImplementedError('Please implement a stepForward function for the environment!')
+
+    def encodeCurrentState(self):
+        raise NotImplementedError('Please implement an encoding of the current state!')
