9eacbddc2e56df5e5d0ca4a076bd025ca400f687
MughilM <mughilan.muthupari@gmail.com>
2021-03-09 20:29:43 -0500

    Started work on main....

--- rl/implementation/main.py
+++ rl/implementation/main.py
@@ -11,3 +11,27 @@ gamma, etc. are also available. The corresponding file from /envs/ is grabbed an
 """
 from .config import *
 from .envs.snake.game import *
+import argparse
+import os
+
+GAMES = {
+    'snake': SnakeQTable
+}
+
+def run():
+    """
+    The main worker method. This will have all the options necessary so that
+    one of the existing reinforcement learning implementations is either
+    trained or played with. The agents could be games or some other thing.
+    The options and the in-built dictionary allow the correct classes
+    to be called based on the arguments (see below)...
+    :return:
+    """
+    pass
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='A program to train/play with reinforcement learning agents.')
+    # Add all the arguments (there are a lot of them...)
+    parser.add_argument('game', choices=['snake'])
+    parser.add_argument()
+
